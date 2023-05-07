eab29e05d6c8c5babba6c552e848a047582bf5ec
Zhao Xin <owenzx@gmail.com>
2023-05-07 09:18:38 +0800

    * changed code base on 2.10.0.

--- Sources/SwiftUICharts/BarChart/Views/SubViews/Bars.swift
+++ Sources/SwiftUICharts/BarChart/Views/SubViews/Bars.swift
@@ -40,8 +40,8 @@ internal struct ColourBar<CD: CTBarChartDataProtocol & GetDataProtocol,
         GeometryReader { geo in
             RoundedRectangleBarShape(chartData.barStyle.cornerRadius)
                 .fill(colour)
-                .frame(width: BarLayout.barWidth(geo.size.width, chartData.barStyle.barWidth))
-                .frame(height: frameAnimationValue(dataPoint.value, height: geo.size.height))
+                .scaleEffect(y: startAnimation ? divideByZeroProtection(CGFloat.self, dataPoint.value, chartData.maxValue) : 0, anchor: .bottom)
+                .scaleEffect(x: chartData.barStyle.barWidth, anchor: .center)
                 .offset(offsetAnimationValue(dataPoint.value, size: geo.size))
                 .background(Color(.gray).opacity(0.000000001))
                 .animation(.default, value: chartData.dataSets)
